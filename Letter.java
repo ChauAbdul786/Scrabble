c57eab14e722e4e296fc43da5f2ee72e6b2b021e
Andrew Shim <andrewcshim@gmail.com>
2023-02-02 15:12:16 -0800

    Added Letter.java

---
+++ Letter.java
@@ -0,0 +1,44 @@
+public class Letter {
+    private char letter;
+    private int points;
+    
+    // The constructor checks the validity of the arguments and throws an 'IllegalArgumentException' if either of them are invalid.
+    public Letter(char letter, int points) {
+      if (!Character.isAlphabetic(letter) || !Character.isUpperCase(letter)) {
+        throw new IllegalArgumentException("Invalid letter: " + letter);
+      }
+      if (points < 0) {
+        throw new IllegalArgumentException("Invalid points: " + points);
+      }
+      this.letter = letter;
+      this.points = points;
+    }
+    
+    public char getLetter() {
+      return letter;
+    }
+    
+    public int getPoints() {
+      return points;
+    }
+    
+    // Returns 'true' if the argument is a 'Letter' object with the same letter and points, and 'false' otherwise.
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) {
+        return true;
+      }
+      if (!(obj instanceof Letter)) {
+        return false;
+      }
+      Letter other = (Letter) obj;
+      return letter == other.letter && points == other.points;
+    }
+    
+    // Returns a string representation of the letter in the format necessary.
+    @Override
+    public String toString() {
+      return "Letter: " + letter + " Points: " + points;
+    }
+}
+  
