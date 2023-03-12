6448a40ff44d2ab6ad2982ea0acf783173d384a9
ChauAbdul786 <ChauAbdul786@gmail.com>
2023-03-11 20:36:09 -0800

    Edited hand layout for readability

--- HandView.java
+++ HandView.java
@@ -4,9 +4,11 @@ import java.awt.event.*;
 
 public class HandView {
     JFrame frame;
+    Hand hand;
 
     public void startHandGUI(JFrame f, Hand h){
         frame = f;
+        hand = h;
         //JLabel testLabel = new JLabel("HAND GOES HERE");
         //testLabel.setFont(new Font("Serif", Font.PLAIN, 48));
         //frame.getContentPane().add(BorderLayout.SOUTH, testLabel);
@@ -16,11 +18,16 @@ public class HandView {
         frame.add(handPanel);
 
         for(int i = 0; i < h.getSize(); i++){
-            JButton button = new JButton(String.valueOf(h.getLetter(i).getLetter()));
+            JButton button = new JButton(String.valueOf(hand.getLetter(i).getLetter()));
+            button.setFont(new Font("Serif", Font.PLAIN, 24));
             handPanel.add(button);
         }
 
-        frame.getContentPane().add(BorderLayout.SOUTH, handPanel);
+        JPanel handContainer = new JPanel();
+        handContainer.setLayout(new GridBagLayout());
+        handContainer.add(handPanel);
+
+        frame.getContentPane().add(BorderLayout.SOUTH, handContainer);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
